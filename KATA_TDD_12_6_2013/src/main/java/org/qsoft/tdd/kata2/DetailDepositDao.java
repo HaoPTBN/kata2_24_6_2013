3dd6b489a34374428eac0427fcd15060150a80de
unknown <haopt@ADR-TUNGVX.qsoft.com.vn>
2013-06-24 14:26:51 +0700

    24-6

---
+++ KATA_TDD_12_6_2013/src/main/java/org/qsoft/tdd/kata2/DetailDepositDao.java
@@ -0,0 +1,16 @@
+package org.qsoft.tdd.kata2;
+
+import ogr.qsoft.tdd.entity.DetailDeposit;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: haopt
+ * Date: 6/19/13
+ * Time: 2:05 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DetailDepositDao {
+    public DetailDeposit save(DetailDeposit detailDeposit) {
+        return null;
+    }
+}
